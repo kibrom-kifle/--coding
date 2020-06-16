c820020af3426b61567610d4b470d11a6bf70ecc
kibrom-kifle <35782209+kibrom-kifle@users.noreply.github.com>
2020-06-17 02:40:57 +0400

    Add files via upload

---
+++ number-guessing.py
@@ -0,0 +1,16 @@
+#created by kibrom kifle
+
+from random import randint
+
+rand = randint(1,10)
+guess = True
+
+while guess:
+    num = int(input("Enter enteger number 1-10: "))
+    if num>rand:
+        print('guess lower')
+    elif num<rand:
+        print('guess higher')
+    if num==rand:
+        print('you got the number')
+        guess=False
